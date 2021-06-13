ffd81f9ed16c82b7efb23f78dde7017ae815607b
simonautran <85454074+simonautran@users.noreply.github.com>
2021-06-13 16:08:39 -0400

    ex24

--- src/main/java/ex24/App.java
+++ src/main/java/ex24/App.java
@@ -14,7 +14,6 @@ public class App {
         System.out.println("Enter two strings and I'll tell you if they are anagrams: ");
         System.out.print("Enter the first string: ");
         String first = input.nextLine();
-
         System.out.print("Enter the second string: ");
         String second = input.nextLine();
 
