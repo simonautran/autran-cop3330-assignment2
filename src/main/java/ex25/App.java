843b1d454cf0f6f05c36813ffcc7cfdded2c980c
simonautran <85454074+simonautran@users.noreply.github.com>
2021-06-13 16:55:57 -0400

    ex25

--- src/main/java/ex25/App.java
+++ src/main/java/ex25/App.java
@@ -6,37 +6,63 @@ package ex25;
 import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter your password: ");
-        String password = in.next();
-        if(passwordValidator(password) < 1){
-            System.out.println("The password "+password+" is a very weak password.");
-        }
-        else if(passwordValidator(password) < 2){
-            System.out.println("The password "+password+" is a weak passsword.");
-        }
-        else if(passwordValidator(password) < 3){
-            System.out.println("The password "+password+" is a strong password.");
-        }
-        else System.out.println("The password "+password+" is a very strong password.");
+        String a = prompt();
+        response(a);
     }
-    public static int passwordValidator(String password){
-        int iPasswordScore = 0;
-        if( password.length() < 8 )
-            return 0;
-        else if( password.length() >= 10 )
-            iPasswordScore += 2;
-        else
-            iPasswordScore += 1;
-        if( password.matches("(?=.*[0-9]).*") )
-            iPasswordScore += 2;
-        if( password.matches("(?=.*[a-z]).*") )
-            iPasswordScore += 2;
-        if( password.matches("(?=.*[A-Z]).*") )
-            iPasswordScore += 2;
-        if( password.matches("(?=.*[~!@#$%^&*()_-]).*") )
-            iPasswordScore += 2;
+    public static String prompt(){
+        System.out.println("Enter a new password");
+        return input();
+    }
+    public static void response(String a){
+        if(passwordValidator(a) <= 1){
+            System.out.println("The password '"+ a + "' is a very weak password.");
+        }
+        else if(passwordValidator(a) == 2 || passwordValidator(a) == 3){
+            System.out.println("The password '"+ a + "' is a weak password.");
+        }
+        else if(passwordValidator(a) == 4){
+            System.out.println("The password '"+ a + "' is a strong password.");
+        }
+        else if(passwordValidator(a) == 5){
+            System.out.println("The password '"+ a + "' is a very strong password.");
+        }
+    }
+    public static int passwordValidator(String a){
+        int returnVal = 0;
+        if(a.length() >= 8){
+            returnVal++;
+        }
+        boolean hasSpecials = false;
+        boolean hasDigits = false;
+        boolean hasLetters = false;
 
-        return iPasswordScore;
+        s.toLowerCase();
+        char[] array1 = a.toCharArray();
+        for(int i = 0; i < a.length(); i++)
+        {
+            if(java.lang.Character.isDigit(array1[i])){
+                hasDigits  = true;
+            }
+            if(java.lang.Character.isLetter(array1[i])){
+                hasLetters = true;
+            }
+            if((array1[i] < 65 && array1[i] > 57) ||  (array1[i] < 127 && array1[i] > 122) || (array1[i] < 97 && array1[i] > 90) || (array1[i] < 48 && array1[i] > 32)){
+                hasSpecials  = true;
+            }
+        }
+        if(hasSpecials == true){
+            returnVal++;
+        }
+        if(hasDigits == true){
+            returnVal++;
+        }
+        if(hasLetters == true){
+            returnVal += 2;
+        }
+        return returnVal;
+    }
+    public static String input(){
+        Scanner sc = new Scanner(System.in);
+        return sc.nextLine();
     }
 }
