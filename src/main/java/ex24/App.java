eb77105747a3f48fa3c99d7575be1d50dfbf6a09
simonautran <85454074+simonautran@users.noreply.github.com>
2021-06-13 16:07:14 -0400

    ex24

--- src/main/java/ex24/App.java
+++ src/main/java/ex24/App.java
@@ -13,24 +13,24 @@ public class App {
 
         System.out.println("Enter two strings and I'll tell you if they are anagrams: ");
         System.out.print("Enter the first string: ");
-        String one = input.nextLine();
+        String first = input.nextLine();
 
         System.out.print("Enter the second string: ");
-        String two = input.nextLine();
+        String second = input.nextLine();
 
  boolean result = isAnagram(word1, word2);
 
         if (result) {
-            System.out.println(one + " and " + two + " are anagrams.");
+            System.out.println(first + " and " + second + " are anagrams.");
         } else {
-            System.out.println(one + " and " + two + " are not anagrams.");
+            System.out.println(first + " and " + second + " are not anagrams.");
         }
     }
 
-  public static boolean isAnagram (String one, String two) {
+  public static boolean isAnagram (String first, String second) {
 
-        char[] string1 = one.toCharArray();
-        char[] string2 = two.toCharArray();
+        char[] string1 = first.toCharArray();
+        char[] string2 = second.toCharArray();
 
         Arrays.sort(string1);
         Arrays.sort(string2);
