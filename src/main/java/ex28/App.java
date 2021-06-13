e48728aa5bf0594d4b1714c1c92485014a33fc05
simonautran <85454074+simonautran@users.noreply.github.com>
2021-06-13 18:08:29 -0400

    ex28

--- src/main/java/ex28/App.java
+++ src/main/java/ex28/App.java
@@ -2,3 +2,21 @@
  *  UCF COP3330 Summer 2021 Assignment 2 Solution
  *  Copyright 2021 Simon Autran
  */
+package ex28;
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int[] array = new int[5];
+        int sum = 0;
+        for (int i = 0; i < 5; i++){
+            System.out.print("Enter a number: ");
+            array[i] = input.nextInt();
+            sum += array[i];
+        } 
+        for (int j = 0;j < 5;j++){
+            sum += array[j];
+        }
+        System.out.println("The total is " + sum + ".");
+    }
+}
