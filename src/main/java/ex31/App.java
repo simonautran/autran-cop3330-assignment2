c791b53bd3c98374a7300d7cb18160d5d6416a65
simonautran <85454074+simonautran@users.noreply.github.com>
2021-06-13 18:46:29 -0400

    ex31

--- src/main/java/ex31/App.java
+++ src/main/java/ex31/App.java
@@ -2,3 +2,21 @@
  *  UCF COP3330 Summer 2021 Assignment 2 Solution
  *  Copyright 2021 Simon Autran
  */
+package ex31;
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("What is your age? ");
+        int age = input.nextInt();
+        System.out.print("What is your resting heart rate? ");
+        int heartrate = input.nextInt();
+        System.out.println("Resting Pulse: 65        Age: 22\n");
+        System.out.println("Intensity\t | Rate");
+        System.out.println("-------------|--------");
+        for (double i = 0.55; i < 1; i = i + .05) {
+            double bpm = (((220 - age) - heartrate) * i) + heartrate;
+            System.out.printf("%.0f%%\t\t\t | %.0f bpm\n", i * 100, bpm);
+        }
+    }
+}
