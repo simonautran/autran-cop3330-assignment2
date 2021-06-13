6669971b70735d3b554c831f0925450e5ba6414e
simonautran <85454074+simonautran@users.noreply.github.com>
2021-06-13 19:24:20 -0400

    ex35

--- src/main/java/ex35/App.java
+++ src/main/java/ex35/App.java
@@ -2,3 +2,26 @@
  *  UCF COP3330 Summer 2021 Assignment 2 Solution
  *  Copyright 2021 Simon Autran
  */
+package ex35;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        ArrayList<String> names = new ArrayList<String>();
+        System.out.print("Enter a name: ");
+        String newName = input.nextLine();
+        if (!newName.equals("")) {
+            names.add(newName);
+        }
+        for (int i = 0; !newName.equals("") ; i++) {
+            System.out.print("Enter a name: ");
+            newName = input.nextLine();
+            if (!newName.equals("")) {
+                names.add(newName);
+            }
+        }
+        int winner = (int) Math.floor(Math.random() * names.size() );
+        System.out.println("The winner is... " + names.get(winner) + ".");
+    }
+}
