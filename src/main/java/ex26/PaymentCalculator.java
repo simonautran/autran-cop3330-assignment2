9c734331970227acbe330f9db683a7c09fa19ef9
simonautran <85454074+simonautran@users.noreply.github.com>
2021-06-13 17:43:57 -0400

    Update PaymentCalculator.java

--- src/main/java/ex26/PaymentCalculator.java
+++ src/main/java/ex26/PaymentCalculator.java
@@ -2,6 +2,15 @@
  *  UCF COP3330 Summer 2021 Assignment 2 Solution
  *  Copyright 2021 Simon Autran
 */
- 
+
 package ex26;
 public class PaymentCalculator {
+public static double PaymentCalculator(double balance, double apr, double monthPayment){
+        double round = balance / monthPayment;
+        round = Math.ceil(round * 100);
+        round = round / 100;
+        double monthcount = Math.log(1 + (rounded) * (1 - Math.pow(1 + apr, 30)));
+        monthcount = monthcount * (-1/30);
+        monthcount = monthcount / Math.log(1 + apr);
+        monthcount = Math.ceil(monthcount);
+        return monthcount;
