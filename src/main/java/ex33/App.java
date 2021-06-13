10437b2d4f19867fbf965ed1e5c0c8008c14262e
simonautran <85454074+simonautran@users.noreply.github.com>
2021-06-13 19:19:21 -0400

    ex33

--- src/main/java/ex33/App.java
+++ src/main/java/ex33/App.java
@@ -2,3 +2,15 @@
  *  UCF COP3330 Summer 2021 Assignment 2 Solution
  *  Copyright 2021 Simon Autran
  */
+package ex33;
+import java.util.Scanner;
+public class App {
+    static String[] arr = {"Yes", "No", "Maybe", "Ask again later."};
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("What's your question? ");
+        String question = input.nextLine();
+        int rand = (int) Math.floor((Math.random() * 5));
+        System.out.print("\n" + arr[rand]);
+    }
+}
