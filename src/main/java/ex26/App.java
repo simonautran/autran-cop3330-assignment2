93ff2ce89645a69217a256eb0fc89abb76eab53e
simonautran <85454074+simonautran@users.noreply.github.com>
2021-06-13 17:21:52 -0400

    ex26

--- src/main/java/ex26/App.java
+++ src/main/java/ex26/App.java
@@ -2,3 +2,18 @@
  *  UCF COP3330 Summer 2021 Assignment 2 Solution
  *  Copyright 2021 Simon Autran
  */
+package ex26;
+import java.util.Scanner;
+public class App extends PaymentCalculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("What is your balance? ");
+        double balance = input.nextInt();
+        System.out.print("What is the APR on the card (as a percent)? ");
+        double APR = (input.nextDouble() / 100) / 365;
+        System.out.print("What is the monthly payment you can make? ");
+        double monthPayment = input.nextInt();
+        double monthPayOff = PaymentCalculator(balance, APR, monthPayment);
+        System.out.printf("It will take you %.0f months to pay off this card.", monthPayOff);
+    }
+}
