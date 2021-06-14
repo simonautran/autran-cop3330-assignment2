643b3b48f76794e570cfb20bc5ee33542fea34a8
simonautran <85454074+simonautran@users.noreply.github.com>
2021-06-13 22:39:58 -0400

    ex29

--- src/main/java/ex29/ex29.java
+++ src/main/java/ex29/ex29.java
@@ -4,7 +4,7 @@
  */
 package ex29;
 import java.util.Scanner;
-public class App {
+public class ex29 {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
