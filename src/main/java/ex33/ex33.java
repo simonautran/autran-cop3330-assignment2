6c1b3358b0de75a111a923a698aa577cd3f79a74
simonautran <85454074+simonautran@users.noreply.github.com>
2021-06-13 22:47:10 -0400

    ex33

--- src/main/java/ex33/ex33.java
+++ src/main/java/ex33/ex33.java
@@ -4,13 +4,15 @@
  */
 package ex33;
 import java.util.Scanner;
-public class App {
+public class ex33 
+{
     static String[] arr = {"Yes", "No", "Maybe", "Ask again later."};
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         Scanner input = new Scanner(System.in);
         System.out.println("What's your question? ");
         String question = input.nextLine();
         int rand = (int) Math.floor((Math.random() * 5));
-        System.out.print("\n" + arr[rand]);
+        System.out.print("\n"+arr[rand]);
     }
 }
