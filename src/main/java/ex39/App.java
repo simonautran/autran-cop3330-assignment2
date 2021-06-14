d480fc41dc63ea86cf1b756f10d1d0b997400ae4
simonautran <85454074+simonautran@users.noreply.github.com>
2021-06-13 21:13:10 -0400

    ex39

--- src/main/java/ex39/App.java
+++ src/main/java/ex39/App.java
@@ -2,3 +2,37 @@
  *  UCF COP3330 Summer 2021 Assignment 2 Solution
  *  Copyright 2021 Simon Autran
  */
+package ex39;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class App{
+
+    public static ArrayList<Employee> sort(ArrayList<Employee> list){
+        Collections.sort(list);
+        return list;
+    }
+
+    public static StringBuilder getReadyForPrinting(ArrayList<Employee> list){
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(getHeader());
+
+        for(Employee employee : list){
+            sb.append(getEmployeeData(employee));
+        }
+
+        return sb;
+    }
+
+    public static String getEmployeeData(Employee employee){
+        return String.format("%-25s| %-30s| %s\n", employee.getFullName(), employee.getPosition(),
+                (employee.getSeparationDate()== null ? "" : employee.getSeparationDate()));
+    }
+
+    public static String getHeader(){
+        return (String.format("%-25s| %-30s| %s\n" +
+                "-----------------------------------------------------------------------------\n",
+                "Name", "Position", "Separation Date"));
+    }
+}
