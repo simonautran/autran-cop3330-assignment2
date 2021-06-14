d3d506416b3725edcc91f15f0296a0ad9b5f7b6f
simonautran <85454074+simonautran@users.noreply.github.com>
2021-06-13 22:45:00 -0400

    ex32

--- src/main/java/ex32/ex32.java
+++ src/main/java/ex32/ex32.java
@@ -2,7 +2,9 @@
  *  UCF COP3330 Summer 2021 Assignment 2 Solution
  *  Copyright 2021 Simon Autran
  */
-public class App 
+package ex32;
+import java.util.Scanner;
+public class ex32 
 {
     public static void main(String[] args) 
     {
