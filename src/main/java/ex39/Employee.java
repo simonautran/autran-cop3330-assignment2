c2031153071be8898f377adccaee7ee5fecc637b
simonautran <85454074+simonautran@users.noreply.github.com>
2021-06-13 21:25:54 -0400

    ex39 Employee

--- src/main/java/ex39/Employee.java
+++ src/main/java/ex39/Employee.java
@@ -1,1 +1,45 @@
-
+/*
+ *  UCF COP3330 Summer 2021 Assignment 2 Solution
+ *  Copyright 2021 Simon Autran
+ */
+package ex39;
+import java.time.LocalDate;
+public class Employee implements Comparable<Employee>{
+    private String firstName;
+    private String lastName;
+    private String fullName;
+    private String position;
+    private LocalDate separationDate;
+    public Employee(String firstName, String lastName, String position, LocalDate separationDate){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + lastName;
+        this.position = position;
+        this.separationDate = separationDate;
+    }
+    public String getFirstName(){
+        return firstName;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+    public String getFullName(){
+        return fullName;
+    }
+    public String getPosition(){
+        return position;
+    }
+    public LocalDate getSeparationDate(){
+        return separationDate;
+    }
+   @Override
+    public int compareTo(Employee employee)
+    {
+        return this.lastName.compareTo(employee.lastName);
+   }
+    @Override
+   public String toString()
+   {
+        return getClass().getSimpleName() + "[" + firstName + ", " + lastName + ", " + position + ", " + separationDate + "]";
+   }
+}
