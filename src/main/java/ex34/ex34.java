cfcfead4636dc9fc27f97e56ec1ac1bd1c3da9a2
simonautran <85454074+simonautran@users.noreply.github.com>
2021-06-13 22:49:38 -0400

    ex34

--- src/main/java/ex34/ex34.java
+++ src/main/java/ex34/ex34.java
@@ -4,28 +4,34 @@
  */
 package ex34;
 import java.util.Scanner;
-public class App {
-    public static void main(String[] args) {
+public class ex34 
+{
+    public static void main(String[] args) 
+    {
         Scanner input = new Scanner(System.in);
         String[] names = { "John Smith", "Jackie Jackson", "Chris Jones", "Amanda Cullen", "Jeremy Goodwin" };
-        System.out.println("There are " + names.length + " employees:");
+        System.out.println("There are " +names.length+ " employees: ");
 
-        for (int i = 0; i < names.length; i++) {
+        for (int i = 0; i < names.length; i++) 
+        {
             System.out.println(names[i]);
         }
         System.out.print("\nEnter an employee name to remove: ");
         String removedName = input.nextLine();
         String[] newNames = new String[names.length - 1];
         int pos = 0;
-        for (int i = 0; i < names.length; i++) {
-            if (!names[i].equals(removedName)) {
+        for (int i = 0; i < names.length; i++) 
+        {
+            if (!names[i].equals(removedName)) 
+            {
                 newNames[pos] = names[i];
                 pos++;
             }
         }
-        System.out.println("\nThere are " + newNames.length + " employees:");
+        System.out.println("\nThere are "+ newNames.length +" employees:");
 
-        for (int i = 0; i < newNames.length; i++) {
+        for (int i = 0; i < newNames.length; i++) 
+        {
             System.out.println(newNames[i]);
         }
     }
